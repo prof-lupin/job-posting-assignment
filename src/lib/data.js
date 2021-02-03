5500b1f8cab55835732082453991e7f48fc7a0e1
arya.shrivastava <arya.shrivastava@kollie.ai>
2021-02-03 10:55:19 +0530

    added dummy data

---
+++ src/lib/data.js
@@ -0,0 +1,66 @@
+export const locationsList = [
+    {
+        id: 1,
+        location: "Pune",
+    },
+    {
+        id: 2,
+        location: "Bangalore",
+    },
+    {
+        id: 3,
+        location: "Mumbai",
+    },
+    {
+        id: 4,
+        location: "Hyderabad",
+    },
+    {
+        id: 5,
+        location: "Delhi-NCR",
+    },
+];
+
+export const categoryList = [
+    {
+        id: 1,
+        category: "Software Developor",
+    },
+    {
+        id: 2,
+        category: "Full Stack Developer",
+    },
+    {
+        id: 3,
+        category: "Frontend Developer",
+    },
+    {
+        id: 4,
+        category: "Backend Developer",
+    },
+];
+
+export const skillsList = [
+    {
+        id: 1,
+        skill: "Java",
+    },
+    {
+        id: 2,
+        skill: "ReactJs",
+    },
+    {
+        id: 3,
+        skill: "NodeJs",
+    },
+    {
+        id: 4,
+        skill: "Python",
+    },
+    {
+        id: 5,
+        skill: "NoSQL",
+    },
+];
+
+
